1e88d491854df55384df91536a62c83db02baae7
princesimform <prince.m@simformsolutions.com>
2023-03-24 17:17:08 +0530

    update view page

--- js/view.js
+++ js/view.js
@@ -3,105 +3,106 @@ import UI from "./Ui.js";
 import { showToast } from "./Toast.js";
 let bookImgObj = {};
 import {
-    imgUpdate, booktitleTag, bookauthorNameTag, bookisbnNoTag, imgInput, updateBook, deleteBook, updateForm,
-    updateFormTitle,
-    updateFormAuthor,
-    updateFormIsbn,
-    updateFormId
+  imgUpdate, booktitleTag, bookauthorNameTag, bookisbnNoTag, imgInput, updateBook, deleteBook, updateForm,
+  updateFormTitle,
+  updateFormAuthor,
+  updateFormIsbn,
+  updateFormId
 } from "./utils.js";
 
 function Onload() {
-    const localList = new LocalList();
-    const url = window.location.search
-    const urlParams = new URLSearchParams(url);
-    const bookId = urlParams.get('id')
-    try {
-        let BookData = localList.getItem(bookId);
-    } catch (error) {
-        window.location.href = "/index.html"
-    }
-    console.log(BookData);
+  const localList = new LocalList();
+  const url = window.location.search
+  const urlParams = new URLSearchParams(url);
+  const bookId = urlParams.get('id')
+  let BookData = [];
+  try {
+    BookData = localList.getItem(bookId);
+  } catch (error) {
+    window.location.href = "/index.html"
+  }
+  console.log(BookData);
 
-    booktitleTag.innerHTML = BookData.title
-    bookauthorNameTag.innerHTML = BookData.author
-    bookisbnNoTag.innerHTML = BookData.isbn
-    imgInput.src = BookData.frontPage
-    updateBook.value = BookData.id
-    deleteBook.value = BookData.id
+  booktitleTag.innerHTML = BookData.title
+  bookauthorNameTag.innerHTML = BookData.author
+  bookisbnNoTag.innerHTML = BookData.isbn
+  imgInput.src = BookData.frontPage
+  updateBook.value = BookData.id
+  deleteBook.value = BookData.id
 
 }
 
 Onload();
 
 updateBook.addEventListener('click', (e) => {
-    const ui = new UI();
-    ui.updateBookList(e.target.value);
+  const ui = new UI();
+  ui.updateBookList(e.target.value);
 })
 
 
 // Update Form
 updateForm.addEventListener('submit', function (e) {
-    const title = updateFormTitle.value,
-        author = updateFormAuthor.value,
-        isbn = updateFormIsbn.value,
-        id = updateFormId.value;
-    let frontPage = {};
+  const title = updateFormTitle.value,
+    author = updateFormAuthor.value,
+    isbn = updateFormIsbn.value,
+    id = updateFormId.value;
+  let frontPage = {};
 
-    let localList = new LocalList();
-    const ui = new UI();
+  let localList = new LocalList();
+  const ui = new UI();
 
-    console.log(frontPage);
-    let oldData = localList.getItem(id);
-    if (oldData.author == author && oldData.isbn == isbn && oldData.title == title && JSON.stringify(bookImgObj) == '{}') {
-        showToast('Please Update Data', 'bg-danger');
+  console.log(frontPage);
+  let oldData = localList.getItem(id);
+  if (oldData.author == author && oldData.isbn == isbn && oldData.title == title && JSON.stringify(bookImgObj) == '{}') {
+    showToast('Please Update Data', 'bg-danger');
+  } else {
+
+    if (JSON.stringify(bookImgObj) == "{}") {
+      frontPage = localStorage.getItem(id)
     } else {
-
-        if (JSON.stringify(bookImgObj) == "{}") {
-            frontPage = localStorage.getItem(id)
-        } else {
-            frontPage = bookImgObj;
-        }
-
-        let reqData = { id: parseInt(id), title: title, author: author, isbn: isbn, frontPage: frontPage }
-        try {
-            localList.updateItem(reqData);
-            showToast('Data Updated', 'bg-primary');
-            Onload();
-            $('#exampleModal').modal('toggle');
-        } catch (error) {
-            console.log(error);
-            showToast('Something Went Wrong', 'bg-danger')
-        }
+      frontPage = bookImgObj;
     }
 
-    e.preventDefault();
+    let reqData = { id: parseInt(id), title: title, author: author, isbn: isbn, frontPage: frontPage }
+    try {
+      localList.updateItem(reqData);
+      showToast('Data Updated', 'bg-primary');
+      Onload();
+      $('#exampleModal').modal('toggle');
+    } catch (error) {
+      console.log(error);
+      showToast('Something Went Wrong', 'bg-danger')
+    }
+  }
+
+  e.preventDefault();
 })
 
 
 imgUpdate.addEventListener('change', function (e) {
-    let imgFile = e.target.files[0];
+  let imgFile = e.target.files[0];
 
-    if (imgFile.type == 'image/png' || imgFile.type == 'image/jpeg' || imgFile.type == 'image/jpg' || imgFile.type == 'image/gif') {
-        const reader = new FileReader();
-        reader.addEventListener("load", function (e) {
-            bookImgObj = reader.result;
-        })
-        reader.readAsDataURL(imgFile)
-    } else {
-        showToast('Please Add Valid Image', 'bg-danger');
-        imgUpdate.value = "";
-    }
+  if (imgFile.type == 'image/png' || imgFile.type == 'image/jpeg' || imgFile.type == 'image/jpg' || imgFile.type == 'image/gif') {
+    const reader = new FileReader();
+    reader.addEventListener("load", function (e) {
+      bookImgObj = reader.result;
+    })
+    reader.readAsDataURL(imgFile)
+  } else {
+    showToast('Please Add Valid Image', 'bg-danger');
+    imgUpdate.value = "";
+  }
 })
 
 deleteBook.addEventListener('click', (e) => {
-    const id = e.target.value;
-    console.log();
-    let localList = new LocalList();
-    try {
-        localList.removeItem(id);
-        window.location.href = '/index.html';
-    } catch (error) {
-        console.log(error);
-        showToast('Something Went Wrong', 'bg-danger')
-    }
+  const id = e.target.value;
+  console.log();
+  let localList = new LocalList();
+  try {
+    localList.removeItem(id);
+    window.location.href = '/index.html';
+  } catch (error) {
+    console.log(error);
+    showToast('Something Went Wrong', 'bg-danger')
+  }
 })
